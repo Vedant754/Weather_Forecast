d526dfe10b5b2b8cb7d8192d972ec75042ca2c94
Vedant754 <149894136+Vedant754@users.noreply.github.com>
2024-06-07 15:34:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,3 @@ if __name__ == "__main__":
     root = tk.Tk()
     app = WeatherApp(root)
     root.mainloop()
-
-
-# echo "# Weather_Forecast" >> README.md
-# git init
-# git add README.md
-# git commit -m "first commit"
-# git branch -M main
-# git remote add origin https://github.com/Vedant754/Weather_Forecasting.git
-# git push -u origin main
