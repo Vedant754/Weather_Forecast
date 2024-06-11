3a2b070799daaf1c3f3f83187def7a22065d9c3c
Vedant754 <149894136+Vedant754@users.noreply.github.com>
2024-06-11 20:37:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-API_KEY = "Your API key here" 
+API_KEY = "your_api_key_here" 
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
 FORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast?"
